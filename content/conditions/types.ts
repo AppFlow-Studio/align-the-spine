24008607e19b8e0abcd32ff775bbb9d1f2ff106f
djuraku1ov <sarik.j2445@gmail.com>
2026-07-23 18:02:28 +0500

    feat: add understanding block to Condition type

--- content/conditions/types.ts
+++ content/conditions/types.ts
@@ -1,5 +1,24 @@
+export interface ConditionType {
+  name: string;
+  description: string;
+}
+
+export interface ConditionRedFlags {
+  title: string;
+  bullets: string[];
+}
+
+export interface ConditionUnderstanding {
+  intro: string;
+  image: { src: string; alt: string };
+  types: ConditionType[];
+  causes: string[];
+  redFlags: ConditionRedFlags;
+}
+
 export interface Condition {
   slug: string;
   name: string;
   summary: string;
+  understanding: ConditionUnderstanding;
 }
